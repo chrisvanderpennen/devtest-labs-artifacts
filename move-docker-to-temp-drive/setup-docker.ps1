abed2ec4e489f1188eee594cab7eb25c783141ae
Chris van der Pennen <chris.vanderpennen@alcidion.com>
2018-03-26 10:49:44 +1030

    fix json escaping, move tmp

--- move-docker-to-temp-drive/setup-docker.ps1
+++ move-docker-to-temp-drive/setup-docker.ps1
@@ -1,6 +1,6 @@
 $content = @'
 {
-    "data-root": "D:\"
+    "data-root": "D:\\"
 }
 '@
 
@@ -10,6 +10,9 @@ $encoding = new-object System.Text.UTF8Encoding -ArgumentList @($false)
 $action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -command "& {if ((get-service docker).Status -ne ''Running'') { Start-Sleep -Seconds 1 }; docker pull microsoft/windowsservercore:latest; docker pull microsoft/nanoserver:latest}"'
 $trigger = New-ScheduledTaskTrigger -AtStartup
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "docker pull" -Description "Pull server core and nanoserver base images"
+
+[System.Environment]::SetEnvironmentVariable('TEMP', 'D:\Temp', 'Machine')
+[System.Environment]::SetEnvironmentVariable('TMP', 'D:\Temp', 'Machine')
 
 restart-service docker
 docker pull microsoft/windowsservercore:latest
