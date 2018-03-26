0dc848870289985515a4fb282bc2a54bcfc6b075
Chris van der Pennen <chris.vanderpennen@alcidion.com>
2018-03-26 10:53:10 +1030

    no spaces in scheduled task names

--- move-docker-to-temp-drive/setup-docker.ps1
+++ move-docker-to-temp-drive/setup-docker.ps1
@@ -9,7 +9,7 @@ $encoding = new-object System.Text.UTF8Encoding -ArgumentList @($false)
 
 $action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -command "& {if ((get-service docker).Status -ne ''Running'') { Start-Sleep -Seconds 1 }; docker pull microsoft/windowsservercore:latest; docker pull microsoft/nanoserver:latest}"'
 $trigger = New-ScheduledTaskTrigger -AtStartup
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "docker pull" -Description "Pull server core and nanoserver base images"
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "docker-pull" -Description "Pull server core and nanoserver base images"
 
 [System.Environment]::SetEnvironmentVariable('TEMP', 'D:\Temp', 'Machine')
 [System.Environment]::SetEnvironmentVariable('TMP', 'D:\Temp', 'Machine')
