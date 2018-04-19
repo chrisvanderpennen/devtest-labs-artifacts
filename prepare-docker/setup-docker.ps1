6e549e4a0c103960b7659000d772dd58aba2b066
Chris van der Pennen <chris.vanderpennen@alcidion.com>
2018-04-19 20:40:34 +0930

    add prepare-docker artifact

---
+++ prepare-docker/setup-docker.ps1
@@ -0,0 +1,4 @@
+set-mppreference -DisableRealtimeMonitoring $true
+docker pull microsoft/windowsservercore:latest
+docker pull microsoft/nanoserver:latest
+docker pull microsoft/dotnet-framework:4.7.1
